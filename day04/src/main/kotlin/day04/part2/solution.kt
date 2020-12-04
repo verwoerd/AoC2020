39f9e083f0b56270fbb4fff6231b6366dfe2d7c6
Thomas Verwoerd <thomas@keystone-strategic.com>
2020-12-04 08:01:53 +0100

    remove unnecessary elvis expressions for range checks

--- day04/src/main/kotlin/day04/part2/solution.kt
+++ day04/src/main/kotlin/day04/part2/solution.kt
@@ -12,16 +12,16 @@ fun part2(input: BufferedReader): Any {
   return parseLines(input).count { it.extraValidation() }
 }
 
-val colorRegex = Regex("^#[\\w\\d]{6}$")
+val colorRegex = Regex("^#[a-f\\d]{6}$")
 val pidRegex = Regex("^\\d{9}$")
 val eyeColours = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 
-fun Passport.extraValidation() = isValid() && byr.toIntOrNull() ?: 0 in 1920..2002 &&
-    iyr.toIntOrNull() ?: 0 in 2010..2020 &&
-    eyr.toIntOrNull() ?: 0 in 2020..2030 &&
+fun Passport.extraValidation() = isValid() && byr.toIntOrNull() in 1920..2002 &&
+    iyr.toIntOrNull() in 2010..2020 &&
+    eyr.toIntOrNull() in 2020..2030 &&
     when (hgt.takeLast(2)) {
-      "cm" -> hgt.dropLast(2).toIntOrNull() ?: 0 in 150..193
-      "in" -> hgt.dropLast(2).toIntOrNull() ?: 0 in 59..76
+      "cm" -> hgt.dropLast(2).toIntOrNull() in 150..193
+      "in" -> hgt.dropLast(2).toIntOrNull() in 59..76
       else -> false
     } && hcl.matches(colorRegex)
     && ecl in eyeColours
