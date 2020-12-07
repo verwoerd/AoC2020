e7d60e3156e1b942a1af7d90e3b9438b3c019261
Thomas Verwoerd <thomas@keystone-strategic.com>
2020-12-07 15:55:00 +0100

    one line part1

--- day07/src/main/kotlin/day07/part1/solution.kt
+++ day07/src/main/kotlin/day07/part1/solution.kt
@@ -6,15 +6,12 @@ import java.io.BufferedReader
  * @author verwoerd
  * @since 07/12/2020
  */
-fun part1(input: BufferedReader): Any {
-  val bags =
-    input.lineSequence().map(Bag.Companion::parseLine).fold(mutableMapOf<String, MutableSet<String>>()) { acc, bag ->
-      bag.contents.map { acc.getOrPut(it.key, { mutableSetOf() }).add(bag.color) }.let { acc }
-    }.withDefault { mutableSetOf() }
-  return bags.getParents("shiny gold").count()
-}
+fun part1(input: BufferedReader): Any =
+  input.lineSequence().map(Bag.Companion::parseLine).fold(mutableMapOf<String, MutableSet<String>>()) { acc, bag ->
+    bag.contents.map { acc.getOrPut(it.key, { mutableSetOf() }).add(bag.color) }.let { acc }
+  }.withDefault { mutableSetOf() }.getParents("shiny gold").count()
 
-fun Map<String,MutableSet<String>>.getParents(current: String, seen: Set<String> = emptySet()): Set<String> =
+fun Map<String, MutableSet<String>>.getParents(current: String, seen: Set<String> = emptySet()): Set<String> =
   seen union getValue(current).filter { it !in seen }.flatMap { getParents(it, seen + it) }
 
 val baseRegex = Regex("^(?<color>[\\w ]+) bags contain (?<contents>.+)\\.$")
